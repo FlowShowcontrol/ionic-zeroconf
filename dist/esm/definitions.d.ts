3cd5e1d4a20849a4b04f5e8c565dd4682b484699
Wouter J <wouter@wjaudio.nl>
2018-05-25 17:27:28 +0200

    added browser

--- dist/esm/definitions.d.ts
+++ dist/esm/definitions.d.ts
@@ -13,3 +13,4 @@ export interface ZeroConfPlugin {
         domain: string;
     }): Promise<any>;
 }
+export declare var Browser: ZeroConfPlugin;
--- dist/esm/definitions.js
+++ dist/esm/definitions.js
@@ -1,1 +1,2 @@
+export var Browser;
 //# sourceMappingURL=definitions.js.map
